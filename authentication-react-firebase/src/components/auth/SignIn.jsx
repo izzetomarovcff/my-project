b7ad157c35cef96382d1757e1612f632b6ff6ccf
izzetomarovcff <izzetomarovcff@gmail.com>
2024-01-23 17:27:50 +0400

    auth-react-firebase

---
+++ authentication-react-firebase/src/components/auth/SignIn.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../firebase'
+const SignIn = () => {
+    const [email,setEmail] = useState ("")
+    const [password,setPassword] = useState ("")
+    const signIn = (e) => {
+        e.preventDefault()
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential)=>{
+            console.log(userCredential)
+            window.location.href = "http://localhost:3000/"
+        }).catch((error)=>{
+            console.log(error)
+        })
+    }
+    const changeRoute = () =>{
+        window.location.href = "http://localhost:3000/signup"
+    }
+  return (
+    <div className='sigin-in-container'>
+        <form onSubmit={signIn}>
+            <h1>Log In To Your Account</h1>
+            <input type="text" name="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Email'/>
+            <input type="password" name="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Password'/>
+            <button type="submit">Log In</button>
+            <button onClick={changeRoute}>Create Account</button>
+        </form>
+    </div>
+  )
+}
+
+export default SignIn
